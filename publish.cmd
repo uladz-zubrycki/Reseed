df3b2dfe3830951f238f4ab011d5be20155cb637
Uladzislau Zubrytski <v.zubritsky@gmail.com>
2021-10-02 19:05:40 +0200

    Include Readme into package

--- publish.cmd
+++ publish.cmd
@@ -5,7 +5,10 @@ SET "key=%NUGET_RESEED_KEY%"
 SET "outpath=%src%\Reseed\bin\publish"
 SET version=0.1.2
 
-rmdir /s /q %outpath%
+if exist %outpath%\ (
+    rmdir /s /q %outpath%
+)
+
 dotnet pack "%src%\Reseed\Reseed.csproj" -p:PackageVersion=%version%  -p:DirectoryBuildPropsPath="%props%" -p:OutputPath=%outpath% 
 if %errorlevel% neq 0 exit /b %errorlevel%
 
