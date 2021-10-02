fcd9edd51133dfb6026b61c49d23a1619970d551
Uladzislau Zubrytski <v.zubritsky@gmail.com>
2021-10-02 18:32:09 +0200

    Add publish script

---
+++ publish.cmd
@@ -0,0 +1,13 @@
+﻿@echo off
+SET "src=%~dp0src"
+SET "props=%~dp0publish.props"
+SET "key=%NUGET_RESEED_KEY%"
+SET "outpath=%src%\Reseed\bin\publish"
+SET version=0.0.2
+
+rmdir /s /q %outpath%
+dotnet pack "%src%\Reseed\Reseed.csproj" -p:PackageVersion=%version%  -p:DirectoryBuildPropsPath="%props%" -p:OutputPath=%outpath% 
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+dotnet nuget push --api-key %key% --source https://api.nuget.org/v3/index.json "%outpath%\Reseed.%version%.nupkg"
+if %errorlevel% neq 0 exit /b %errorlevel%
